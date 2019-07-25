1388f7c765f0599bc7f9118f44f7c66d821a9f05
Cláudio Hilário <claudio.hilario@ontech.pt>
2019-07-26 00:59:42 +0100

    [ADD] Tipos

---
+++ tipos/tipos.js
@@ -0,0 +1,17 @@
+"use strict";
+// String
+var nome = 'Claudio';
+console.log(nome);
+//nome = 28;
+// numbers
+var idade = 27;
+//idade = 'Teste';
+idade = 27.5;
+//Boolean
+var isBoolean = true;
+//isBoolean = 1
+isBoolean = false;
+// Tipos explísitos 
+var myAge;
+myAge = 27;
+console.log(typeof myAge);
---
+++ tipos/tipos.ts
@@ -0,0 +1,20 @@
+// String
+let nome: string = 'Claudio';
+console.log(nome);
+//nome = 28;
+
+// numbers
+
+let idade: number = 27;
+//idade = 'Teste';
+idade = 27.5;
+
+//Boolean
+let isBoolean: boolean = true;
+//isBoolean = 1
+isBoolean = false;
+
+// Tipos explísitos 
+let myAge: any
+myAge = 27;
+console.log(typeof myAge);

268448ee9d7751b0af09057d041c62fd06415c03
Cláudio Hilário <claudio.hilario@ontech.pt>
2019-07-30 01:02:37 +0100

    [ADD] Class Produto test

--- classes/classes.js
+++ classes/classes.js
@@ -7,6 +7,9 @@ var Data = /** @class */ (function () {
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
     }
     return Data;
 }());
@@ -17,4 +20,39 @@ console.log(aniversario);
 var casamento = new Data; //Podese omitir os ()
 casamento.ano = 2019;
 console.log(casamento);
+var DataEsperta = /** @class */ (function () {
+    function DataEsperta(dia, mes, ano) {
+        if (dia === void 0) { dia = 1; }
+        if (mes === void 0) { mes = 1; }
+        if (ano === void 0) { ano = 1970; }
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    return DataEsperta;
+}());
+var aniversarioEsperto = new DataEsperta(29, 5, 1992);
+aniversarioEsperto.dia = 1;
+console.log(aniversarioEsperto.dia);
+console.log(aniversarioEsperto);
+var casamentoEsperto = new DataEsperta; //Podese omitir os ()
+casamentoEsperto.ano = 2019;
+console.log(casamentoEsperto);
+// Desafio
+var Produto = /** @class */ (function () {
+    function Produto(nome, preco, desconto) {
+        if (desconto === void 0) { desconto = 0; }
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+    return Produto;
+}());
+var produto1 = new Produto('Nome produto 1', 30, 15);
+console.log(produto1);
+var produto2 = new Produto('Nome produto 1', 30);
+console.log(produto2);
 //# sourceMappingURL=classes.js.map
--- classes/classes.ts
+++ classes/classes.ts
@@ -1,9 +1,6 @@
 class Data {
-    dia: number;
-    public mes: number;
-    ano: number;
 
-    constructor(dia: number = 1, mes: number = 1, ano: number = 1970) {
+    constructor(public dia: number = 1, public mes: number = 1, public ano: number = 1970) {
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
@@ -18,3 +15,41 @@ console.log(aniversario);
 const casamento = new Data; //Podese omitir os ()
 casamento.ano = 2019;
 console.log(casamento);
+
+class DataEsperta {
+    dia: number;
+    public mes: number;
+    ano: number;
+
+    constructor(dia: number = 1, mes: number = 1, ano: number = 1970) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
+
+const aniversarioEsperto = new DataEsperta(29, 5, 1992);
+aniversarioEsperto.dia = 1;
+console.log(aniversarioEsperto.dia);
+console.log(aniversarioEsperto);
+
+const casamentoEsperto = new DataEsperta; //Podese omitir os ()
+casamentoEsperto.ano = 2019;
+console.log(casamentoEsperto);
+
+// Desafio
+
+class Produto {
+    constructor(public nome: string, public preco: number, public desconto: number = 0) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+}
+
+const produto1 = new Produto('Nome produto 1', 30, 15);
+console.log(produto1);
+
+const produto2 = new Produto('Nome produto 1', 30);
+console.log(produto2);
+
